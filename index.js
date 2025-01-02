a66ebc85b6705eb0cb057d33a3e9507d13693bd9
MithunGowda <mithungowdahc12a@gmail.com>
2025-01-02 21:47:24 +0530

    day 2

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const app = express()
 app.listen(5000, (req, res)=>{
     console.log(`Server is running at 5000`)
 })
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+    console.log("hello world")
+})
